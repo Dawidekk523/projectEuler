e0ec5786b61c0c483ff2b8b93da6ad68e8fe1351
dawidekk523 <daveedsgn@gmail.com>
2023-02-24 05:58:37 +0100

    ToImprove

---
+++ Problem12.js
@@ -0,0 +1,24 @@
+let fibo = 0
+
+function dzialanie(max, maxDzielnik) {
+    for (let liczba = 1; liczba < max; liczba++) {
+
+        fibo = fibo + liczba
+        let dzielnikcounter = 1
+        for (let dzielnik = 1; dzielnik < maxDzielnik; dzielnik++) {
+            if (fibo % dzielnik == 0) {
+
+                dzielnikcounter = dzielnikcounter + 1
+
+                if(dzielnikcounter == 500){
+                    return console.log(fibo);
+                }
+            }
+            
+        }
+        
+    }
+}
+
+
+dzialanie(10000000, 1000000)
